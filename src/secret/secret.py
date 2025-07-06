514b9bcb5a30bc7a54564aeeeb9f284f9cc5c1ff
naup96321 <naup96721@gmail.com>
2025-07-06 11:12:27 +0800

    add CTFd verify api key

--- src/secret/secret.py
+++ src/secret/secret.py
@@ -1,1 +1,7 @@
-TOKEN_SECRET_KEY = b"TomorinIsCute123"
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="secret/secret.env")
+
+TOKEN_SECRET_KEY = os.getenv("TOKEN_SECRET_KEY").encode()
+CTFd_API_KEY = os.getenv("CTFD_API_KEY")
--- src/usertoken/verify_ctfd.py
+++ src/usertoken/verify_ctfd.py
@@ -1,2 +1,4 @@
+import secret.secret as sec_mod
+
 def verify_ctfd_token(ctfd_token):
     pass
